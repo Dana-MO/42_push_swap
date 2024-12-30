6050284f80eca4974cf427a0ab15a9036c67dfa3
Dana Omar <danaomaark@gmail.com>
2024-12-30 13:31:35 +0300

    update, add stack_init

--- main.c
+++ main.c
@@ -2,6 +2,18 @@
 
 int     main(int argc, char **argv)
 {
+    t_stack     *a;
+    t_stack     *b;
+
+    a = NULL;
+    b = NULL;
+    // clean up validate input file
     validate_input(argc, argv);
+    stack_init(&a, argc, argv);
+    while (a)
+    {
+        printf("Number: %d\n", a->value);
+        a = a->next;
+    }
     return (0);
 }
