5794a931d5f9f74cb67d7a999a7290be43fa9707
Dana Omar <danaomaark@gmail.com>
2024-12-30 13:30:58 +0300

    update header, add struct

--- push_swap.h
+++ push_swap.h
@@ -3,6 +3,12 @@
 
 #include "libft/libft.h"
 
-void    validate_input(int argc, char **argv);
+typedef struct n_stack
+{
+	int             value;
+	struct n_stack	*next;
+}		t_stack;
 
+void    validate_input(int argc, char **argv);
+void    stack_init(t_stack **a, int argc, char **argv);
 #endif
