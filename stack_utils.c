f76ff780ed78f91efc11599d7c2639e7aff715b6
Dana Omar <danaomaark@gmail.com>
2025-01-06 15:10:59 +0300

    modify parameter name

--- stack_utils.c
+++ stack_utils.c
@@ -1,13 +1,13 @@
 #include "push_swap.h"
 
 /* Stack Length */
-int     ft_stacklen(t_stack **a)
+int     ft_stacklen(t_stack **stack)
 {
     int len;
     t_stack *temp;
 
     len = 0;
-    temp = *a;
+    temp = *stack;
     while (temp != NULL)
     {
         len++;
