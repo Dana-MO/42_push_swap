b2699ad52be104a9938b6c697018d3950b9ac117
Dana Omar <danaomaark@gmail.com>
2025-01-09 13:39:37 +0300

    remove move_b_to_a function

--- sort_multiple.c
+++ sort_multiple.c
@@ -5,8 +5,6 @@ static void rotate_both(t_stack **a, t_stack **b, t_stack *cheapest_node)
     while (*b != cheapest_node->target_node
         && *a != cheapest_node)
         rr(a, b);
-    //current_index(*a);
-    //current_index(*b);
 }
 
 static void rev_rotate_both(t_stack **a, t_stack **b, t_stack *cheapest_node)
@@ -14,8 +12,6 @@ static void rev_rotate_both(t_stack **a, t_stack **b, t_stack *cheapest_node)
     while (*b != cheapest_node->target_node
         && *a != cheapest_node)
         rrr(a, b);
-    //current_index(*a);
-    //current_index(*b);
 }
 
 void    prep_for_push(t_stack **stack, t_stack *top_node, char stack_name)
@@ -41,9 +37,6 @@ void    prep_for_push(t_stack **stack, t_stack *top_node, char stack_name)
 
 static void     move_a_to_b(t_stack **a, t_stack **b, t_stack *cheapest_node)
 {
-    //t_stack *cheapest_node;
-
-    //cheapest_node = get_cheapest(*a);
     if (cheapest_node->above_median && cheapest_node->target_node->above_median)
         rotate_both(a, b, cheapest_node);
     else if (!(cheapest_node->above_median)
@@ -52,12 +45,6 @@ static void     move_a_to_b(t_stack **a, t_stack **b, t_stack *cheapest_node)
     prep_for_push(a, cheapest_node, 'a');
     prep_for_push(b, cheapest_node->target_node, 'b');
     pb(a, b);
-}
-
-static void     move_b_to_a(t_stack **a, t_stack **b)
-{
-    prep_for_push(a, (*b)->target_node, 'a');
-    pa(b, a);
 }
 
 static void     min_on_top(t_stack **a)
@@ -91,8 +78,8 @@ void    sort_multiple(t_stack **a, t_stack **b, int stack_len)
     while (*b)
     {
         init_b_nodes(*a, *b);
-        move_b_to_a(a, b);
+        prep_for_push(a, (*b)->target_node, 'a');
+        pa(b, a);
     }
-    //current_index(*a);
     min_on_top(a);
 }
